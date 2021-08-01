7cbe8b0b37e9009af7da8deebc6594c1e534a5af
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-08-01 22:13:04 +0530

    designing web page with css

--- app.py
+++ app.py
@@ -127,9 +127,10 @@ def prediction():
         description = generate_desc(model, tokenizer, photo, max_length)
         size = len(description)
         description = description[:size-3]
+        description=description.upper()
 
     return render_template('index.html', description=description, img_path=img_path)
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True)
