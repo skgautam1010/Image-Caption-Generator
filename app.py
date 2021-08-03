ed97af65dd0c0a70192e13db2cc7cac609429afa
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2021-08-03 15:29:20 +0530

    final commit

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 from flask import Flask, render_template, request
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
-#from keras_applications.imagenet_utils import _obtain_input_shape
 import numpy as np
 from PIL import Image
-#import matplotlib.pyplot as plt
-#import argparse
-from PIL.ImagePalette import load
-from keras.models import Model, load_model
-from pickle import dump, load
-#import string
-#import os
-#from tqdm import tqdm
+from keras.models import load_model
+from pickle import load
 from keras.applications.xception import Xception, preprocess_input
-#from keras.preprocessing.image import load_img, img_to_array
-#from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-#from keras.utils import to_categorical
-#from keras.layers.merge import add
-#from keras.layers import Input, Dense, LSTM, Embedding, Dropout
-
 
 def extract_features(filename, model):
     try:
